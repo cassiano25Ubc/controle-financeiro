94272f0217a8c37584b8c413ec6153bfeabe46b2
Cassiano Maia <cassiano25.ubc@gmail.com>
2026-06-18 22:26:18 -0300

    estrutura inicial do App de finanças

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import GlobalStyle from "./styles/global";
+const App =() => {
+    return (
+        <>
+    <div>App</div>
+    <GlobalStyle/>
+    <h1></h1>
+    </>
+    )
+    
+};
+ export default App
---
+++ src/styles/global.jsx
@@ -0,0 +1,11 @@
+import{createGlobalStyle} from"styled-components"
+const Global = createGlobalStyle`
+
+    body){
+    margin:0
+    padding:0
+    font-family:"poppins",sans-serif
+    background-color:#f2f2f2
+}
+`
+export default Global
